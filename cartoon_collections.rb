9230a92d2e5e7216a388e00f2037ed3abdd181b4
M01-M01 <marlonsaddress@gmail.com>
2018-05-20 19:02:58 -0400

    done

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,18 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(roll)
+  (roll.each_with_index.map {|dwarf, number| "#{number + 1}*#{dwarf}"}).each do |numbered_name|
+    puts numbered_name
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(ingredients)
+  ingredients.collect {|ingredient|  "#{ingredient.capitalize}!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(list)
+  list.any? {|stuff| stuff.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(maybe_cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  (maybe_cheese & cheese_types)[0]
 end
